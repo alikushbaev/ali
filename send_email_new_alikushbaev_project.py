7286870e625669577a53c897525dec2c53f2f183
alikushbaev <149851671+alikushbaev@users.noreply.github.com>
2024-03-09 22:27:21 +0500

    Add files via upload

---
+++ send_email_new_alikushbaev_project.py
@@ -0,0 +1,21 @@
+# from twilio.rest import Client
+# twilio_number = "+12567436668"
+# my_number="+998990883881"
+# client=Client("ACc37222e30197400ff05ff42739ae5925a","ob3f93e1080c8e677d52b90c3600313a")
+# call=client.calls.create(url="https://demo.twilio.com/docs/voice.xml",to=my_number,from_=twilio_number)
+# print(call.sid)
+import smtplib
+from email.mime.text import MIMEText
+def send_email(message,password,sender,to):
+    server = smtplib.SMTP("smtp.gmail.com",587)
+    server.starttls()
+    try:
+        server.login(sender,password)
+        msg = MIMEText(message)
+        msg["Subject"] = "Code"
+        msg["To"] = to
+        server.sendmail(sender,to,msg.as_string())
+        print(f"send successful")
+    except Exception as _ex:
+        print(f"error:{_ex}")
+# send_email("mr","amgxjzuoxjeacfet","iskandarovadilnoza250187@gmail.com","jamshidkushbaev@gmail.com")
